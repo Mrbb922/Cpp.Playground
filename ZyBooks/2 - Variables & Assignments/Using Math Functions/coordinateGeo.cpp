9ced68cd346b8fdca8b97b194970def1b66040e6
Mrbb922 <36170409+Mrbb922@users.noreply.github.com>
2020-05-06 02:23:56 -0400

    Update coordinateGeo.cpp

--- ZyBooks/2 - Variables & Assignments/Using Math Functions/coordinateGeo.cpp
+++ ZyBooks/2 - Variables & Assignments/Using Math Functions/coordinateGeo.cpp
@@ -1,3 +1,11 @@
+/*
+	Determine the distance between point (x1, y1) and point (x2, y2),
+	and assign the result to pointsDistance. The calculation is:
+	Distance = SquareRootOf( (x2 - x1)^2 + (y2 - y1)^2 )
+	You may declare additional variables.
+	Ex: For points (1.0, 2.0) and (1.0, 5.0), pointsDistance is 3.0.
+*/
+
 #include <iostream>
 #include <cmath>
 using namespace std;
