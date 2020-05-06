7a046ffbbfd7223963c4eb29f095035592d4a2b8
Mrbb922 <36170409+Mrbb922@users.noreply.github.com>
2020-05-06 02:01:29 -0400

    Create coordinateGeo.cpp

---
+++ ZyBooks/2 - Variables & Assignments/Using Math Functions/coordinateGeo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+   double x1;
+   double y1;
+   double x2;
+   double y2;
+   double xDist;
+   double yDist;
+   double pointsDistance;
+
+   xDist = 0.0;
+   yDist = 0.0;
+   pointsDistance = 0.0;
+
+   cin >> x1;
+   cin >> y1;
+   cin >> x2;
+   cin >> y2;
+
+   /* Your solution goes here  */
+   
+   double item1 = (x2 - x1);
+   double item2 = (y2 - y1);
+   double item1Pow = pow(item1, 2);
+   double item2Pow = pow(item2, 2);
+   
+   pointsDistance = sqrt(item1Pow + item2Pow);
+   
+   /* End solution */
+
+   cout << pointsDistance << endl;
+
+   return 0;
+}
