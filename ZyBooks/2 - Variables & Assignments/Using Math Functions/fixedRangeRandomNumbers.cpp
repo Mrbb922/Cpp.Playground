c54db416bc0b7db90c606f7b689ea16081707c3a
Mrbb922 <36170409+Mrbb922@users.noreply.github.com>
2020-05-06 21:44:11 -0400

    Create fixedRangeRandomNumbers.cpp

---
+++ ZyBooks/2 - Variables & Assignments/Using Math Functions/fixedRangeRandomNumbers.cpp
@@ -0,0 +1,29 @@
+/*
+
+Type two statements that use rand() to print 2 random integers between (and including) 100 and 149. End with a newline.
+
+Note: For this activity, using one statement may yield different output (due to the compiler calling rand() in a different order).
+Use two statements for this activity. Also, srand() has already been called; do not call srand() again. 
+
+*/
+
+#include <iostream>
+#include <cstdlib>   // Enables use of rand()
+#include <ctime>     // Enables use of time()
+using namespace std;
+
+int main() {
+   int seedVal;
+
+   cin >> seedVal;
+   srand(seedVal);
+
+   /* Your solution goes here  */
+   
+   cout << rand() % 50 + 100 << endl;
+   cout << rand() % 50 + 100 << endl;
+   
+   /* End Solution */
+
+   return 0;
+}
