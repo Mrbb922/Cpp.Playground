d6b66985d48fa787468bde2db0dd2f8927f2980e
Mrbb922 <36170409+Mrbb922@users.noreply.github.com>
2020-05-15 04:20:02 -0400

    Create usingBool.cpp

---
+++ ZyBooks/3 - Branches & Loops/usingBool.cpp
@@ -0,0 +1,30 @@
+/*
+
+      Assign isTeenager with true if kidAge is 13 to 19 inclusive. Otherwise, assign isTeenager with false. 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   bool isTeenager;
+   int kidAge;
+
+   cin >> kidAge;
+
+   /* Your solution goes here  */
+   
+   isTeenager = ((kidAge >= 13) && (kidAge <= 19));
+   
+   /* End Solution */
+
+   if (isTeenager) {
+      cout << "Teen" << endl;
+   }
+   else {
+      cout << "Not teen" << endl;
+   }
+
+   return 0;
+}
