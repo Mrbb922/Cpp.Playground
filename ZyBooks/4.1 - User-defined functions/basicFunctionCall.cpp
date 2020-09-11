28953e95d1e43da8a93772050b98ae4cfb393e08
Mrbb922 <36170409+Mrbb922@users.noreply.github.com>
2020-09-11 06:54:30 -0400

    Create basicFunctionCall.cpp

---
+++ ZyBooks/4.1 - User-defined functions/basicFunctionCall.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void OutputMinutesAsHours(double origMinutes) {
+
+   /* Your solution goes here  */
+   
+   double hours = origMinutes / 60.0;
+   cout << hours;
+   
+   /* End solution */
+
+}
+
+int main() {
+   double minutes;
+
+   cin >> minutes;
+
+   OutputMinutesAsHours(minutes); // Will be run with 210.0, 3600.0, and 0.0.
+   cout << endl;
+
+   return 0;
+}
